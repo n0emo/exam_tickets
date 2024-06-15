91f5ca843b909e5a2d88c98688b2103040d1938f
Albert Shefner <solaris945@gmail.com>
2024-06-15 17:51:35 +0300

    Implement checking each file

--- Математика/build.py
+++ Математика/build.py
@@ -2,20 +2,34 @@
 
 import os
 
-files = [f'{i}.md' for i in range(42, 45)]
+files = [f'{i}.md' for i in range(0, 57)]
 
 with open('all.md', 'w') as all_md:
     all_md.write('\\pagebreak')
     for file in files:
-        with open(file, 'r') as f:
-            content = f.read()
-        all_md.write(content)
-        all_md.write('\n\\pagebreak\n')
+        try:
+            with open(file, 'r') as f:
+                if os.system(
+                    f'pandoc {file} -o {file}.pdf '
+                    '-t latex --pdf-engine=xelatex '
+                    '-V mainfont="Liberation Serif" -V fontsize=12pt '
+                ) != 0:
+                    raise ValueError()
+                os.remove(f'{file}.pdf')
+                content = f.read()
+            all_md.write(content)
+            all_md.write('\n\n\\pagebreak\n\n')
+        except ValueError:
+            print(f"Error in {file}")
+        except:
+            pass
 
 os.system(
-    'pandoc -t latex -o all.pdf --pdf-engine=xelatex '
-    'all.md -V mainfont="Liberation Serif" '
-    '--toc --toc-depth=1 --number-sections'
+    'pandoc all.md -o all.pdf '
+    '-t latex --pdf-engine=xelatex '
+    '-V mainfont="Liberation Serif" -V fontsize=12pt '
+    '-V geometry:margin=1in '
+    '--toc --toc-depth=1 --number-sections '
 )
 
 os.remove('all.md')
