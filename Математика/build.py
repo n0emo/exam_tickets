3dd297db3515593d5e6231adb3ccd833a1789108
Albert Shefner <solaris945@gmail.com>
2024-06-15 16:43:24 +0300

    Fix build.py for fing Windows CMD

--- Математика/build.py
+++ Математика/build.py
@@ -12,11 +12,11 @@ with open('all.md', 'w') as all_md:
         all_md.write(content)
         all_md.write('\n\\pagebreak\n')
 
-os.system('''
-    pandoc -t latex -o all.pdf --pdf-engine=xelatex \\
-    all.md -V mainfont="Liberation Serif" \\
-    --toc --toc-depth=1 --number-sections
-''')
+os.system(
+    'pandoc -t latex -o all.pdf --pdf-engine=xelatex '
+    'all.md -V mainfont="Liberation Serif" '
+    '--toc --toc-depth=1 --number-sections'
+)
 
 os.remove('all.md')
 
